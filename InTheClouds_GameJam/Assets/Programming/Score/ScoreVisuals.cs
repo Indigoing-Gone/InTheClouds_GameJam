9757cb29fabaf30033e40bfd19844d51063ffa84
GarrettJesWade <GarrettJesWade@gmail.com>
2026-02-22 10:37:06 -0500

    [ADD] Updated start and end panels

--- InTheClouds_GameJam/Assets/Programming/Score/ScoreVisuals.cs
+++ InTheClouds_GameJam/Assets/Programming/Score/ScoreVisuals.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class ScoreVisuals : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI totalScoreText;
+    [SerializeField] private TextMeshProUGUI finalScoreText;
 
     public void UpdateTotalScore(int _totalScore)
     {
         totalScoreText.text = $"{_totalScore}";
+        finalScoreText.text = $"{_totalScore}";
     }
 }
